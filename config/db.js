c17227596098a79e4a042bf89d82f0e6d7f8c8db
Shantanu-io <166125364+shantanujambhule@users.noreply.github.com>
2025-06-26 11:34:04 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ if (cached.conn) return cached.conn;
 if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => 
      mongoose);
-    };
+    ;
 }
 try{    
 cached.conn = await cached.promise;
@@ -17,3 +17,4 @@ console.error("Error connecting to MongoDB:", error);
 }
 return cached.conn;
 
+}
